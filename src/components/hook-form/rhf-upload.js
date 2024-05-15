09520a251f076ef3e7a75bc09665fab7df094202
Saeed99Madi <saed.zooro@gmail.com>
2024-05-16 01:25:36 +0300

    create Overtime page and its compoents form components responsive hook-form

---
+++ src/components/hook-form/rhf-upload.js
@@ -0,0 +1,109 @@
+import PropTypes from 'prop-types';
+import { useFormContext, Controller } from 'react-hook-form';
+// @mui
+import FormHelperText from '@mui/material/FormHelperText';
+//
+// import { UploadAvatar, Upload, UploadBox } from '../upload';
+
+// ----------------------------------------------------------------------
+
+export function RHFUploadAvatar({ name, ...other }) {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <div>
+          {/* <UploadAvatar error={!!error} file={field.value} {...other} />
+
+          {!!error && (
+            <FormHelperText error sx={{ px: 2, textAlign: 'center' }}>
+              {error.message}
+            </FormHelperText>
+          )} */}
+           <p>hello</p>
+        </div>
+      )}
+    />
+  );
+}
+
+RHFUploadAvatar.propTypes = {
+  name: PropTypes.string,
+};
+
+// ----------------------------------------------------------------------
+
+export function RHFUploadBox({ name, ...other }) {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        // <UploadBox files={field.value} error={!!error} {...other} />
+        <p>hello</p>
+      )}
+    />
+  );
+}
+
+RHFUploadBox.propTypes = {
+  name: PropTypes.string,
+};
+
+// ----------------------------------------------------------------------
+
+export function RHFUpload({ name, multiple, helperText, ...other }) {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) =>
+        multiple ? (
+          // <Upload
+          //   multiple
+          //   accept={{ 'image/*': [] }}
+          //   files={field.value}
+          //   error={!!error}
+          //   helperText={
+          //     (!!error || helperText) && (
+          //       <FormHelperText error={!!error} sx={{ px: 2 }}>
+          //         {error ? error?.message : helperText}
+          //       </FormHelperText>
+          //     )
+          //   }
+          //   {...other}
+          // />
+          <p>hello</p>
+        ) : (
+          // <Upload
+          //   accept={{ 'image/*': [] }}
+          //   file={field.value}
+          //   error={!!error}
+          //   helperText={
+          //     (!!error || helperText) && (
+          //       <FormHelperText error={!!error} sx={{ px: 2 }}>
+          //         {error ? error?.message : helperText}
+          //       </FormHelperText>
+          //     )
+          //   }
+          //   {...other}
+          // />
+          <p>hello</p>
+        )
+      }
+    />
+  );
+}
+
+RHFUpload.propTypes = {
+  helperText: PropTypes.string,
+  multiple: PropTypes.bool,
+  name: PropTypes.string,
+};
